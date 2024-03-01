b324c15a564c870f6dfab07f9bcf644d02134002
cristian.arango <cristian.arango@dms.ms>
2024-03-01 08:54:24 -0500

    Utilizando un servicio para el consumo de los EndPoints

---
+++ ProyectoFinal.Core/Interfaces/IServices/IExecuteStoredProcedureServiceService.cs
@@ -0,0 +1,14 @@
+﻿using ProyectoFinal.Core.DTOs.Response;
+using System.Data.SqlClient;
+
+namespace ProyectoFinal.Core.Interfaces.IServices
+{
+    public interface IExecuteStoredProcedureServiceService
+    {
+        Task<ResponseDTO> ExecuteStoredProcedure(string storedProcedureName, object parameters);
+        Task<ResponseDTO> ExecuteDataStoredProcedure<TResult>(string storedProcedureName, object parameters, Func<SqlDataReader, List<TResult>> mapFunction);
+        Task<ResponseDTO> ExecuteData<TResult>(string storedProcedureName, Func<SqlDataReader, List<TResult>> mapFunction);
+        Task<ResponseDTO> ExecuteSingleObjectStoredProcedure<TResult>(string storedProcedureName, object parameters, Func<SqlDataReader, TResult> mapFunction);
+    }
+}
+
---
+++ ProyectoFinal.Core/Interfaces/IServices/ISqlCommandService.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.SqlClient;
+
+namespace ProyectoFinal.Core.Interfaces.IServices
+{
+    public interface ISqlCommandService
+    {
+        void AddParameters<T>(SqlCommand command, T parameters);
+    }
+}
